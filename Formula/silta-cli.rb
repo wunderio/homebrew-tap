bc5547fbe08f12b77e187744b29c3cc4b2f87956
Rasmus Werling <rasmus.werling@gmail.com>
2023-05-12 11:44:13 +0300

    Code style fixes, removed reduntant "version" line since brew detects it automatically from the URL.

--- Formula/silta-cli.rb
+++ Formula/silta-cli.rb
@@ -1,23 +1,21 @@
 class SiltaCli < Formula
-  desc "CI/CD deployment command abstraction, utilities and tools for Silta."
+  desc "CI/CD deployment command abstraction, utilities and tools for Silta"
   homepage "https://github.com/wunderio/silta-cli"
   url "https://github.com/wunderio/silta-cli/archive/refs/tags/0.1.24.tar.gz"
   sha256 "501d5743e6285f6c887f62e5a0bcbfdd1e1431dfa2dce16ceeebbad0f9d02f8c"
-  license ""
-  version "0.1.24"
 
-   depends_on "go" => :build
+  depends_on "go" => :build
 
   def install
     system "go", "mod", "download"
     system "go", "build", "-a", *std_go_args(output: "#{bin}/silta")
   end
-  
-    def caveats
-      <<~EOS
-        silta-cli been installed as "silta".
-      EOS
-    end
+
+  def caveats
+    <<~EOS
+      silta-cli been installed as "silta".
+    EOS
+  end
 
   test do
     system "false"
